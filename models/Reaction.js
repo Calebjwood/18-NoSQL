c0c6ab118be8b2be35c127db886b54f399fc0bf1
Caleb Wood <Calebwjwood60@gmail.com>
2023-10-22 06:30:55 -0500

    finished all models

---
+++ models/Reaction.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reactionID: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+);
+
+module.exports = reactionSchema;
